07ce90a5880bba0be20b921ef88c76ea5a48bf3e
David Dvořák <dvorakdavid7@gmail.com>
2026-02-10 10:28:08 +0100

    laravel starter setup

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('Test', [
+        'name' => 'David',
+    ]);
+});
